eb4ca4bdb66f7273cc033b77582cf0dd66d4043b
AREZA13 <88317106+AREZA13@users.noreply.github.com>
2024-02-28 13:27:29 +0300

    Time: 6 ms (63.40%) | Memory: 20.2 MB (8.65%) - LeetSync

---
+++ 66-plus-one/plus-one.php
@@ -0,0 +1,23 @@
+class Solution {
+
+    /**
+     * @param Integer[] $digits
+     * @return Integer[]
+     */
+    function plusOne($digits) {   for ($i = count($digits) - 1; $i >= 0; --$i) {
+
+        if ($digits[$i] == 9) {
+            $digits[$i] = 0;
+            if (!isset($digits[$i - 1])) {
+                array_unshift($digits, 1);
+                break;
+            }
+        } else {
+            $digits[$i] = $digits[$i] + 1;
+            break;
+        }
+    }
+
+    return $digits;
+}
+}
