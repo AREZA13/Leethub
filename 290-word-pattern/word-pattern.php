8c11a4ae711e68a6a412eb32303194d0ece39686
AREZA13 <88317106+AREZA13@users.noreply.github.com>
2024-03-19 17:42:41 +0300

    Time: 3 ms (83.56%) | Memory: 20 MB (50.69%) - LeetSync

--- 290-word-pattern/word-pattern.php
+++ 290-word-pattern/word-pattern.php
@@ -7,29 +7,18 @@ class Solution {
      */
     function wordPattern($pattern, $s)  {
     $patternToStringArray = str_split($pattern);
-    var_dump($patternToStringArray);
     $sToArray = explode(" ", $s);
-//    $oneLetterArray = [];
-//    foreach ($sToArray as $word) {
-//        $oneLetterArray [] = substr($word, 0, 1);
-//    }
-    var_dump($sToArray);
-    $search = [];
-    $search2 = [];
-    foreach ($sToArray as $letter){
-        $search [] = array_search($letter, $sToArray);
-    }
-    foreach ($patternToStringArray as $letter2){
-        $search2 [] = array_search($letter2, $patternToStringArray);
-    }
-    var_dump($search2);
-    var_dump($search);
-    var_dump(array_diff($search, $search2));
-    $arraysAreEqual = $search === $search2;
-    var_dump($arraysAreEqual);
-    if ($arraysAreEqual) {
-        return true;
-    } return false;
 
+    return $this -> arrayOfLetterToKeysNumber($sToArray) === $this -> arrayOfLetterToKeysNumber($patternToStringArray);
+
+}
+
+function arrayOfLetterToKeysNumber($array): array
+{
+    $search = [];
+    foreach ($array as $letter) {
+        $search [] = array_search($letter, $array);
+    }
+    return $search;
 }
 }
